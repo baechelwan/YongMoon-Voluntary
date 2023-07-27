9431d3c83abe51cb3f6b5e7f5b765b296e242279
baechelwan <140359574+baechelwan@users.noreply.github.com>
2023-07-27 12:44:22 +0900

    Update forStudent.js

--- Day4-JsGame/RockScissorsPaper/forStudent.js
+++ Day4-JsGame/RockScissorsPaper/forStudent.js
@@ -105,12 +105,13 @@ const changeIMG = (answer) => { // randomNumber로부터 정해진 answer, 즉 �
     }
 };
 
-const randomNumber = (n, m) => { // n부터 m까지의 랜덤한 숫자를 만드는 난수 발생 함수를 만드세요! return 타입은 INT입니다!
+const randomNumber = (n, m) =>
+{ // n부터 m까지의 랜덤한 숫자를 만드는 난수 발생 함수를 만드세요! return 타입은 INT입니다!
 	/* 
      * n에는 시작 값이 들어오고, m에는 끝 값이 들어오게 되며
      * n~m까지의 난수를 발생시키는 random 함수를 만드세요! (Tip. 2일차 JavaScript 기초 파일의 27페이지를 확인해보세요!)
-     * return 타입은 정수 타입인 INT입니다!
-     */
+     * return 타입은 정수 타입인 INT입니다! */
+    return Math.floor(Math.random() * (m - n +1)) + n;
 };
 
 const judge = (answer, submit) => { // 결과를 "Draw", "Lose", "Win" 형태로 도출하여 String의 형태로 return하세요!
@@ -119,6 +120,36 @@ const judge = (answer, submit) => { // 결과를 "Draw", "Lose", "Win" 형태로
      * answer에는 상대편 카드로는 -1, 0, 1 중 하나의 값이 들어옵니다.
      * submit에는 자신이 선택한 카드에 따라 -1, 0, 1 중 하나의 값이 들어옵니다.
      * answser와 submit의 값을 비교하여 자신이 비겼는지, 졌는지, 이겼는지 판단하는 함수를 만드세요! 
-     * return 타입은 문자열인 String이며, "Draw", "Lose", "Win" 중 하나로 return되게 하면 됩니다!
-     */
-};
+     * return 타입은 문자열인 String이며, "Draw", "Lose", "Win" 중 하나로 return되게 하면 됩니다! */
+    if(submit == answer)
+    {
+        return "Draw";
+    }
+    else if(submit != answer)
+    {
+        if( submit == -1 && answer == 1 )
+        {
+            return "Win";
+        }
+        else if( submit == 0 && answer == -1 )
+        {
+            return "Win";
+        }
+        else if( submit == 1 && answer == 0 )
+        {
+            return "Win";
+        }
+        else if( submit == -1 && answer == 0 )
+        {
+            return "Lose";
+        }
+        else if( submit == 0 && answer == 1 )
+        {
+            return "Lose";
+        }
+        else if( submit == 1 && answer == -1 )
+        {
+            return "Lose";
+        }
+    }
+};  
